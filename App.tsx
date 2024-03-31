ebf406542e4ccab89e9ce1f5ecf2830029871568
Paldeep Singh <paldeepsingh21@gmail.com>
2024-03-31 17:03:52 +0800

    Add album to home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+
+import { albums } from "./assets/albums.json";
+import { Album } from "./components/Album";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Album name={albums[0].name} images={albums[0].images} />
       <StatusBar style="auto" />
+      <TouchableHighlight style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Start slideshow</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
+    gap: 20,
+  },
+  text: {
+    color: "white",
+  },
+  button: {
+    borderRadius: 5,
+    backgroundColor: "white",
+    width: "50%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "blue",
   },
 });
