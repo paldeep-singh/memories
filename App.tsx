157e7fcdaff27317cdca5ff58cc744fc276362db
Paldeep Singh <paldeepsingh21@gmail.com>
2024-04-01 14:56:14 +0800

    Use safeareaview

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 import { albums } from "./assets/albums.json";
 import { colours } from "./colours/colours";
@@ -8,11 +8,11 @@ import { Showcase } from "./components/Showcase";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Album name={albums[0].name} images={albums[0].images} />
-      <StatusBar style="auto" />
       <Showcase images={albums[0].images} name={albums[0].name} />
-    </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
