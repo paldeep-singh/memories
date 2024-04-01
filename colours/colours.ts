ade083f1cf824a18c4321279d99180d89a939785
Paldeep Singh <paldeepsingh21@gmail.com>
2024-04-01 13:33:53 +0800

    Add colours

---
+++ colours/colours.ts
@@ -0,0 +1,13 @@
+export const colours = {
+  "Tyrian purple": "#620e3a",
+  Moonstone: "#47a8bd",
+  Maize: "#f5e663",
+  "Misty rose": "#ffd9da",
+  Charcoal: "#233d4d",
+  "Tropical indigo": "#998fc7",
+  Poppy: "#df2935",
+  "Baby powder": "#fdfffc",
+  "Orange (web)": "#faa916",
+  "Payne's grey": "#475B63",
+  Gunmetal: "#2C3531"
+};
